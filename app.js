bad65f3eb067043331a84aa8158139852a68fc06
Isaac Assegai <isaac.a.travers@gmail.com>
2015-04-23 21:33:36 -0700

    changed db url

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/canvasTest');
+//var db = monk('canvasTest:micheale@ds061308.mongolab.com:61308/canvastest');
+//mongodb://<dbuser>:<dbpassword>@ds061308.mongolab.com:61308/canvastest
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
