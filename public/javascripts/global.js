9ecf46e84267049944c52b887eea7ece28ecc67b
Isaac Assegai <isaac.a.travers@gmail.com>
2015-04-25 01:36:58 -0700

    basic graph transversal is working

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -28,7 +28,8 @@ function registerClicks(){
 function calculateClicked(timePassed){
 	var powerSupplies = scope.powerSuppliesList;
 	//alpha algorithm
-	for(var i = 0; i < powerSupplies.length; i++) {
+	//for(var i = 0; i < powerSupplies.length; i++) {
+	for(var i = 0; i < 1; i++) {
 		var supply = powerSupplies[i];
 		var node = getNodeFromPowerSupply(supply);
 		//var node = nodes.filter(function(v) {
@@ -45,6 +46,8 @@ function calculateClicked(timePassed){
 			supply.totalPower = 0;
 		}
 		
+		calculateNeighbourNodes(node);
+		
 	}
 	scope.$apply(function(){
 		//scope.powerSuppliesList = powerSupplies;
@@ -57,6 +60,121 @@ function getNodeFromPowerSupply(powerSupply){
 	    return v._id === powerSupply.nodeID; // filter out appropriate one
 	})[0];
 	return node;
+}
+
+function getNeighboursFromNode(node){
+	var neighbourIDS = node.neighbours;
+	var neighbours = getNeighboursFromNeighbourIDS(neighbourIDS);
+	return neighbours;
+}
+
+function getNeighboursFromNeighbourIDS(neighbourIDS){
+	var neighbours = [];
+	for(var i = 0; i < neighbourIDS.length; i++){
+		var id = neighbourIDS[i];
+		var newNode = getNodeFromID(id);
+		if(!newNode.calculated){ //this stops us from unlimited loops of the graph
+			neighbours.push(getNodeFromID(id));
+		}
+	}
+	return neighbours;
+}
+
+function getNodeFromID(id){
+	var node = scope.nodesList.filter(function(v) {
+	    return v._id === id; // filter out appropriate one
+	})[0];
+	return node;
+}
+
+function calculateNeighbourNodes(node){
+	//alpha.2.C
+	var neighbours = getNeighboursFromNode(node);
+	if(neighbours.length == 0)return; //there are no neighbors to calculate
+	var neighbourPower = node.interimPower[node.interimPower.length -1] / neighbours.length;
+	//alpha.2.D
+	var nnode;
+	for(var i = 0; i < neighbours.length; i++){
+		nnode = neighbours[i];
+		nnode.interimPower.push(neighbourPower);
+	}
+	//alpha.2.D.insert
+	node.interimPower.pop();
+	node.calculated = true;
+	//alpha.2.E
+	for(var i = 0; i < neighbours.length; i++){
+		nnode = neighbours[i];
+		//if this node is attached to a tLine, calculate the other nodes info
+		//alpha.1.E.II
+		if(nnode.belongsToLine){
+			//calculate Power Transmission
+			var nodeB = getOtherTLineNode(nnode);
+			transferPower(nnode, nodeB);
+			nnode.calculated = true;
+			calculateNeighbourNodes(nodeB);
+		}else if(nnode.belongsToConsumer){
+			alert("nnode belongstoConsumer "+nnode._id);
+			//calculate n
+			//here is where we will make the consumer have power
+			nnode.calculated = true;
+		}else{
+			calculateNeighbourNodes(nnode);
+			nnode.calculated = true;
+		}
+	}
+}
+
+//returns the OTHER node connected to this node
+function getOtherTLineNode(node){
+	var tLineID = getTLineIDFromNode(node);
+	var tLine = getTLineFromID(tLineID);
+	var nodes = getNodesFromTLine(tLine);
+	var otherNode = 0;
+	for(var i = 0; i < nodes.length; i++){
+		if(nodes[i]._id != node._id){
+			otherNode = nodes[i];
+		}
+	}
+	return otherNode;
+}
+
+//transfers power from nodeA to nodeB based on the tLine inbetween.
+function transferPower(nodeA, nodeB){
+	var transferPower = nodeA.interimPower[nodeA.interimPower.length -1]/2;
+	nodeB.interimPower.push(transferPower);
+	nodeA.interimPower[nodeA.interimPower.length -1] = nodeA.interimPower[nodeA.interimPower.length -1] - transferPower;
+}
+
+//returns a tline from our data with a matching id
+function getTLineFromID(id){
+	var tLine = scope.transmissionLinesList.filter(function(v) {
+	    return v._id === id; // filter out appropriate one
+	})[0];
+	return tLine;
+}
+
+//returns the nodes associated to a tLine
+function getNodesFromTLine(tLine){
+	var nodes = [];
+	var nodeAID = tLine.nodeAID;
+	var nodeBID = tLine.nodeBID;
+	var nodeA = getNodeFromID(nodeAID);
+	var nodeB = getNodeFromID(nodeBID);
+	nodes.push(nodeA);
+	nodes.push(nodeB);
+	return nodes;
+}
+
+function getTLineIDFromNode(node){
+	var nodeID = node._id;
+	var tLines = scope.transmissionLinesList;
+	var id = 0;
+	for(var i = 0; i < tLines.length; i++){
+		if(tLines[i].nodeAID == node._id || tLines[i].nodeBID == node._id){
+			id = tLines[i]._id;
+		}
+	}
+	return id;
 }
 
 
