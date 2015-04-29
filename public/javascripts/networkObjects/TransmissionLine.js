2c514065ef2ea856ce0098fb67d203d8b3e2b045
Isaac Assegai <isaac.a.travers@gmail.com>
2015-04-28 17:58:23 -0700

    added getters and setters for transmissionLine

--- public/javascripts/networkObjects/TransmissionLine.js
+++ public/javascripts/networkObjects/TransmissionLine.js
@@ -13,11 +13,53 @@ function TransmissionLine (p){
 	this.chargeSet = p.chargeSet;
 }
 TransmissionLine.prototype = {    
-		constructor: TransmissionLine, 
+		constructor: TransmissionLine,
+		getResistor:function(){
+			return this.resistor;
+		},
+		setResistor:function(resistor){
+			this.resistor = resistor;
+		},
+		getDParam:function(){
+			return this.dParam;
+		},
+		setDParam:function(dParam){
+			this.dParam = dParam;
+		},
+		getKParam:function(){
+			return this.kParam;
+		},
+		setKParam:function(kParam){
+			this.kParam = kParam;
+		},
+		getConA:function(){
+			return this.conA;
+		},
+		setConA:function(conA){
+			this.conA = conA;
+		},
+		getConB:function(){
+			return this.conB;
+		},
+		setConB:function(conB){
+			this.conB = conB;
+		},
 		getChargedSet:function(){
 			return this.prototype.getChargedSet();
 		},
 		setChargedSet:function(chargedSet){
 			this.prototype.setChargedSet(chargedSet);
+		},
+		setID:function(id){
+			this._id = id;
+		},
+		getID:function(){
+			return this._id;
+		},
+		getType:function(){
+			return this.type;
+		},
+		setType:function(type){
+			this.type = type;
 		}
 };
